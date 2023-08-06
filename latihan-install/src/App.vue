4080aebd58509f7b7f34c5fb224fc40fdea5884b
jejevj <wijaya.angga.j@gmail.com>
2023-08-07 02:02:25 +0700

    first commit

---
+++ latihan-install/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" v-model="name">
+  <h4>Name: {{ name }}</h4>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      name:""
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
